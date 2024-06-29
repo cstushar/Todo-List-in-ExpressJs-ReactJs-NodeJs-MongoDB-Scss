877127e0debe70ac8b3f517e77d94079ea014596
Tushar Gupta <cstushar18@gmail.com>
2024-06-29 13:02:15 +0530

    first commit

--- Server/index.js
+++ Server/index.js
@@ -1,41 +1,54 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const TodoModel = require('./Models/Todo')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const TodoModel = require('./Models/Todo');
+const dotenv = require('dotenv');
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+// Load environment variables from .env file
+dotenv.config();
 
-mongoose.connect('mongodb://127.0.0.1:27017/TodoList')
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Use MongoDB URL from .env file
+const mongoUrl = process.env.MONGO_URL;
+
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 
 app.get('/get', (req, res) => {
     TodoModel.find()
     .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+    .catch(err => res.json(err));
+});
 
 app.put('/update/:id', (req, res) => {
-    const {id} = req.params;
-    TodoModel.findByIdAndUpdate({_id: id}, {done: true})
+    const { id } = req.params;
+    TodoModel.findByIdAndUpdate({ _id: id }, { done: true })
     .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+    .catch(err => res.json(err));
+});
 
 app.delete('/delete/:id', (req, res) => {
-    const {id} = req.params;
-    TodoModel.findByIdAndDelete({_id: id})
+    const { id } = req.params;
+    TodoModel.findByIdAndDelete({ _id: id })
     .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+    .catch(err => res.json(err));
+});
 
 app.post('/add', (req, res) => {
-    const task =req.body.task;
+    const task = req.body.task;
     TodoModel.create({
         task: task
     }).then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+    .catch(err => res.json(err));
+});
+
 app.listen(3001, () => {
-    console.log("Server is running on port 3001")
-})
+    console.log("Server is running on port 3001");
+});
